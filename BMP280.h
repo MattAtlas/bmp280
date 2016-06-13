428317a6ad0ac70df3512cf40c69fa82bd6a6f77
MattAtlas <saltattam@gmail.com>
2016-06-13 16:31:51 -0700

    Update BMP280.h

--- BMP280.h
+++ BMP280.h
@@ -1,10 +1,14 @@
 #ifndef BMP280_H
 #define BMP280_H
 
+int read_bmp280();
+
 float get_temperature();
 float get_pressure();
+float get_altitude();
 
 int readCoefficients();
-float readAltitude();
+
+int read_bmp280_when_ready(int timeout);
 
 #endif
